bfe68e8b399ab5ba08c1b30f58457e643c3fa89d
Sergey Dyatlov <sergey.dyatlov@exposit.com>
2022-09-12 16:19:40 +0300

    added translation

--- src/pages/ToDoList/ToDoList.tsx
+++ src/pages/ToDoList/ToDoList.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ToDoListItem from '../../components/ToDoListItem/ToDoListItem';
 import { Todo } from '../../interface/todo';
 import { useGetTodoList } from '../../hooks/todo-list-hooks';
+import { useTranslation } from 'react-i18next';
 
 const ToDoList = () => {
     const todos = useGetTodoList();
+    const { t } = useTranslation();
 
     return (
         <div className="overflow-auto mt-2 w-100">
@@ -14,7 +16,7 @@ const ToDoList = () => {
                         todos.length ? todos.map((todo: Todo) => {
                             return <ToDoListItem key={todo.id} todo={todo}/>;
                         }) :
-                            <h2>There is no any data</h2>
+                            <h2>{ t('NO_DATA') }</h2>
                     }
                 </div>
             </div>
